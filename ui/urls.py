d29474ccb01b1e2f64529957120eac360deb4c8a
mkkekkonen2 <mkkekkonen@protonmail.com>
2020-11-26 01:22:16 +0200

    Login/logout functionality + initialize DB

---
+++ ui/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+  path('login/', LoginView.as_view(template_name='auth/login.html')),
+  path('logout/', LogoutView.as_view(next_page='/')),
+]
